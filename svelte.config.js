2a874f9971059774224ebcef51b31568a9068c0e
Cameron Wisniewski <cjjwisniewski@gmail.com>
2025-03-21 17:23:49 -0400

    modify fonts

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,6 @@
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import azure from 'svelte-adapter-azure-swa';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -11,7 +12,7 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you're not sure, use a different adapter.
 		// https://kit.svelte.dev/docs/adapters#supported-environments
-		adapter: adapter()
+		adapter: azure()
 	}
 };
 
